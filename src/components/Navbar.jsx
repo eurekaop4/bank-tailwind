4516b439c542d26f7cea899ed85ea068378c2765
eurekaop4 <kewinmask3@hotmail.com>
2022-08-09 19:56:45 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import { MdAccountBalance } from "react-icons/md";
+import { FaBars, FaGithub, FaLinkedinIn, FaTimes } from "react-icons/fa";
+
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div className="w-full max-w-[1000px] mx-auto flex justify-between">
+        <div className="text-red-500 cursor-pointer">
+          <Link to="home" smooth={true} duration={500}>
+            <MdAccountBalance size={30} />
+          </Link>
+        </div>
+
+        {/* menu */}
+        <div>
+          <ul className=" hidden md:flex">
+            <li>
+              <Link to="home" smooth={true} duration={500}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="about" smooth={true} duration={500}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="skills" smooth={true} duration={500}>
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link to="work" smooth={true} duration={500}>
+                Work
+              </Link>
+            </li>
+            <li>
+              <Link to="contact" smooth={true} duration={500}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* hamburger */}
+      <div className="md:hidden z-10" onClick={handleClick}>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* social icon */}
+      <div className="hidden md:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-center items-center ml-[-100px] hover:ml-[0px] duration-300 bg-blue-500">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              LinkedIn <FaLinkedinIn size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-center items-center ml-[-100px] hover:ml-[0px] duration-300 bg-pink-500">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-center items-center ml-[-100px] hover:ml-[0px] duration-300 bg-blue-500">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              LinkedIn <FaLinkedinIn size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-center items-center ml-[-100px] hover:ml-[0px] duration-300 bg-pink-500">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
